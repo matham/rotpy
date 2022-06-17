068850ae3e5016fb10f539b8b35fbb175c2afab9
Matthew Einhorn <matt@einhorn.dev>
2022-06-16 20:31:23 -0400

    Add mac wheels.

--- setup.py
+++ setup.py
@@ -28,20 +28,26 @@ def get_wheel_data():
         items.extend(map(str, root.glob('*LICENSE')))
 
         data.append(('share/rotpy/spinnaker/cti', items))
-        data.append(('share/rotpy/spinnaker/bin', items))
 
     return data
 
 
 include_dirs = []
 library_dirs = []
+libraries = []
 
 if sys.platform in ('win32', 'cygwin'):
-    libraries = ['Spinnaker_v140', ]
+    libraries.append('Spinnaker_v140')
 else:
-    libraries = ['Spinnaker']
+    libraries.append('Spinnaker')
+
     include_dirs.append('/opt/spinnaker/include')
+    include_dirs.append('/usr/local/include/spinnaker')
+    include_dirs.append('/Applications/Spinnaker/include')
+
     library_dirs.append('/opt/spinnaker/lib')
+    library_dirs.append('/usr/local/lib')
+    library_dirs.append('/Applications/Spinnaker/lib')
 
 include = environ.get('ROTPY_INCLUDE')
 if include:
