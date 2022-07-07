a15b24c0e5ceb013a323a721c4efda444ce3d0a3
Matthew Einhorn <matt@einhorn.dev>
2022-07-07 13:33:07 -0400

    Bump version to v0.2.0.

--- rotpy/__init__.py
+++ rotpy/__init__.py
@@ -4,7 +4,7 @@
 Project that provides python bindings for the Spinnaker SDK and provides
 access to their GigE and USB cameras.
 """
-__version__ = '0.1.1.dev0'
+__version__ = '0.2.0'
 
 import sys
 import site
