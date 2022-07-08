a5b8062c2fa2f1c75870b5ede206ff7992e78238
Matthew Einhorn <matt@einhorn.dev>
2022-07-07 21:13:56 -0400

    Don't add unneeded paths to the path.

--- rotpy/__init__.py
+++ rotpy/__init__.py
@@ -22,10 +22,29 @@ It is read only.
 
 _bitness = '64' if sys.maxsize > 2 ** 32 else '32'
 
-_dirs = [sys.prefix, site.USER_BASE]
-if hasattr(sys, '_MEIPASS'):
-    _dirs.append(sys._MEIPASS)
-for d in _dirs:
+
+def _set_flir_path(root, add_path=True):
+    if os.path.exists(os.path.join(root, 'FLIR_GenTL.cti')):
+        os.environ[f'FLIR_GENTL{_bitness}_CTI'] = os.path.join(
+            root, 'FLIR_GenTL.cti')
+    if os.path.exists(os.path.join(root, 'FLIR_GenTL_v140.cti')):
+        os.environ[f'FLIR_GENTL{_bitness}_CTI_VS140'] = os.path.join(
+            root, 'FLIR_GenTL_v140.cti')
+
+    _name = f'GENICAM_GENTL{_bitness}_PATH'
+    if _name in os.environ:
+        os.environ[_name] = root + os.pathsep + os.environ[_name]
+    else:
+        os.environ[_name] = root
+
+    if add_path:
+        os.environ["PATH"] = root + os.pathsep + os.environ["PATH"]
+        if hasattr(os, 'add_dll_directory'):
+            os.add_dll_directory(root)
+        dep_bins.append(root)
+
+
+for d in [sys.prefix, site.USER_BASE]:
     p = join(d, 'share', 'rotpy', 'spinnaker', 'bin')
     if os.path.isdir(p):
         os.environ["PATH"] = p + os.pathsep + os.environ["PATH"]
@@ -33,22 +52,9 @@ for d in _dirs:
             os.add_dll_directory(p)
         dep_bins.append(p)
 
-    for p in (join(d, 'share', 'rotpy', 'spinnaker', 'cti'), d):
-        if os.path.isdir(p):
-            if os.path.exists(os.path.join(p, 'FLIR_GenTL.cti')):
-                os.environ[f'FLIR_GENTL{_bitness}_CTI'] = os.path.join(
-                    p, 'FLIR_GenTL.cti')
-            if os.path.exists(os.path.join(p, 'FLIR_GenTL_v140.cti')):
-                os.environ[f'FLIR_GENTL{_bitness}_CTI_VS140'] = os.path.join(
-                    p, 'FLIR_GenTL_v140.cti')
+    p = join(d, 'share', 'rotpy', 'spinnaker', 'cti')
+    if os.path.isdir(p):
+        _set_flir_path(p)
 
-            _name = f'GENICAM_GENTL{_bitness}_PATH'
-            if _name in os.environ:
-                os.environ[_name] = p + os.pathsep + os.environ[_name]
-            else:
-                os.environ[_name] = p
-
-            os.environ["PATH"] = p + os.pathsep + os.environ["PATH"]
-            if hasattr(os, 'add_dll_directory'):
-                os.add_dll_directory(p)
-            dep_bins.append(p)
+if hasattr(sys, '_MEIPASS') and os.path.isdir(sys._MEIPASS):
+    _set_flir_path(sys._MEIPASS, False)
